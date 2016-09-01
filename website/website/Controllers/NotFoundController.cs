fbb5059bd1f3774281417a2ac2ccd2a60564d88f
dmschrispena <cpena@dmshouston.com>
2016-08-31 18:30:55 -0600

    Initial

---
+++ website/website/Controllers/NotFoundController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace website.Content
+{
+    public class NotFoundController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
